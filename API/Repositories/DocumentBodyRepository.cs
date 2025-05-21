49fff950990f5b91e6f20843e4092f834247ba20
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-05-21 21:38:11 +0700

    feat: Adding repository (Review, DocumentBody)

---
+++ API/Repositories/DocumentBodyRepository.cs
@@ -0,0 +1,35 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public class DocumentBodyRepository
+    {
+        private static readonly List<DocumentBody> _documentBodies = new List<DocumentBody>();
+
+        public static List<DocumentBody> DocumentBodies
+        {
+            get => _documentBodies;
+            set
+            {
+                if (value != null)
+                {
+                    _documentBodies.Clear();
+                    _documentBodies.AddRange(value);
+                }
+            }
+        }
+
+        public static DocumentBody? GetDocumentBodyById(Guid id)
+        {
+            return _documentBodies.FirstOrDefault(db => db.Id == id);
+        }
+
+        public static void AddDocumentBody(DocumentBody documentBody)
+        {
+            if (documentBody != null)
+            {
+                _documentBodies.Add(documentBody);
+            }
+        }
+    }
+}
---
+++ API/Repositories/ReviewRepository.cs
@@ -0,0 +1,32 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public class ReviewRepository
+    {
+        private static readonly List<Review> _reviews = new List<Review>();
+        public static List<Review> Reviews
+        {
+            get => _reviews;
+            set
+            {
+                if (value != null)
+                {
+                    _reviews.Clear();
+                    _reviews.AddRange(value);
+                }
+            }
+        }
+        public static Review? GetReviewById(Guid id)
+        {
+            return _reviews.FirstOrDefault(r => r.Id == id);
+        }
+        public static void AddReview(Review review)
+        {
+            if (review != null)
+            {
+                _reviews.Add(review);
+            }
+        }
+    }
+}
