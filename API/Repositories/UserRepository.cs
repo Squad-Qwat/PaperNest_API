409db2be7aefbd34b4c3a0fc83d59c6964b0ecee
Calderaly <calderaly16@gmail.com>
2025-05-21 20:18:27 +0700

    feat: Adding Document, User, and Workspace Repositories

---
+++ API/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public class UserRepository
+    {
+        public static List<User> userRepository = new List<User>();
+
+        public static List<UserWorkspace> userWorkspaceRepository = new List<UserWorkspace>();
+    }
+}
---
+++ API/Repositories/WorkspaceRepository.cs
@@ -0,0 +1,9 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public class WorkspaceRepository
+    {
+        public static List<Workspace> workspaceRepository = new List<Workspace>();
+    }
+}
