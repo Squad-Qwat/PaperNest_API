c1c7b1e51c0c84d492bdba73dcfaa6686dab9238
Calderaly <calderaly16@gmail.com>
2025-05-22 07:52:20 +0700

    feat: Add UserController

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+using API.Services;
+
+namespace PaperNest_API.Controllers
+{
+    [ApiController, Route("/api/users")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            var user = UserService.GetAll();
+
+            return Ok(new
+            {
+                message = "Success get all User data",
+                data = user
+            });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(Guid id)
+        {
+            var user = UserService.GetById(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new
+            {
+                message = "Success get all User data with id: " + user.Id,
+                data = user
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(Guid id, [FromBody] User user)
+        {
+            var existingUser = UserService.GetById(id);
+
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            existingUser.Name = user.Name;
+            existingUser.Username = user.Username;
+
+            return Ok(new
+            {
+                message = "Success to update user with id: " + existingUser.Id
+            });
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteUser(Guid id)
+        {
+            UserService.Delete(id);
+
+            return Ok(new
+            {
+                message = "Berhasil menghapus user dengan id " + id
+            });
+        }
+    }
+}
