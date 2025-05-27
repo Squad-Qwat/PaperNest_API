725f52ff575d97964e7fc07805d297d82c05a4db
Tdarrelpa <darrel2208@gmail.com>
2025-05-28 06:17:04 +0700

    What's this about:
    Add revision to dev, need to integrate a thing or two from dev-caldera branch first before doing pull request

--- API/Helpers/CitationFormatter.cs
+++ API/Helpers/CitationFormatter.cs
@@ -1,34 +1,51 @@
+using API.Helpers.Enums;
 using API.Models;
 
 namespace API.Helpers
 {
     public static class CitationFormatter
     {
+        // Menggunakan aturan format sitasi dengan teknik stair-step table-driven
+        private static readonly CitationFormatRule[] _formattingRules =
+        {
+            new CitationFormatRule
+            {
+                Type = CitationType.Book,
+                Formatter = citation => $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. {citation.PublicationInfo}."
+            },
+            new CitationFormatRule
+            {
+                Type = CitationType.JournalArticle,
+                Formatter = citation => $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. {citation.PublicationInfo}."
+            },
+            new CitationFormatRule
+            {
+                Type = CitationType.Website,
+                Formatter = citation => $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. {citation.PublicationInfo}. Diakses dari {citation.AccessDate}"
+            },
+            new CitationFormatRule
+            {
+                Type = CitationType.ConferencePaper,
+                Formatter = citation => $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. In {citation.PublicationInfo}."
+            },
+            new CitationFormatRule
+            {
+                Type = CitationType.Thesis,
+                Formatter = citation => $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. {citation.PublicationInfo}."
+            }
+        };
+
         public static string GenerateAPAStyle(Citation citation)
         {
-            string apaString = "";
-            switch (citation.Type)
+            foreach (var rule in _formattingRules)
             {
-                case Enums.CitationType.Book:
-                    apaString = $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. {citation.PublicationInfo}.";
-                    break;
-                case Enums.CitationType.JournalArticle:
-                    apaString = $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. {citation.PublicationInfo}.";
-                    break;
-                case Enums.CitationType.Website:
-                    apaString = $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. {citation.PublicationInfo}. Diakses dari {citation.AccessDate}";
-                    break;
-                case Enums.CitationType.ConferencePaper:
-                    apaString = $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. In {citation.PublicationInfo}.";
-                    break;
-                case Enums.CitationType.Thesis:
-                    apaString = $"{citation.Author}. ({citation.PublicationDate?.Year}). {citation.Title}. {citation.PublicationInfo}.";
-                    break;
-                default:
-                    apaString = "Format sitasi tidak didukung.";
-                    break;
+                if (rule.Type == citation.Type)
+                {
+                    return rule.Formatter(citation);
+                }
             }
-            return apaString;
+            // Default case if no rule matches
+            return "Format sitasi tidak didukung.";
         }
     }
 }
---
+++ API/Helpers/Enums/CitationFormatRule.cs
@@ -0,0 +1,10 @@
+﻿using API.Models;
+
+namespace API.Helpers.Enums
+{
+    public class CitationFormatRule
+    {
+        public CitationType Type { get; set; }
+        public required Func<Citation, string> Formatter { get; set; }
+    }
+}
